e1c7715bd0626e0002513edfe75543e76bec6cd0
Pranav Shukla <pranav@thoughtwin.com>
2025-02-18 18:08:29 +0530

    Rollback tocken expiry rediect

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
 
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
     try {
-        const token = request.headers.get('Authorization')?.split(' ')[1];
-
+        const token = request.cookies.get('token'); 
         if (!token) {
-            // return NextResponse.redirect(new URL('/accounts/login', request.url));
-            return response
-        }
-
-        const SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET || 'secret';
-
-        try {
-            jwt.verify(token, SECRET_KEY);
-        } catch (error) {
-            if (error instanceof jwt.TokenExpiredError) {
-                // Token is expired, clear the cookie and redirect to login
-                response.cookies.delete('token');
-                return NextResponse.redirect(new URL('/accounts/login', request.url));
-            }
-            // return NextResponse.json({ error: 'Unauthordfized' }, { s`tatus: 401 });
             return NextResponse.redirect(new URL('/accounts/login', request.url));
         }
 
